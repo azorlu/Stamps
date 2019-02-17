0eac2bcaed47b993dfe49f49a43529d2d38092ec
azorlu <mandelbug@gmail.com>
2019-02-17 11:49:48 +0900

    Added Categories Controller

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stamps.Controllers.Resources;
+using Stamps.Models;
+using Stamps.Persistence;
+
+namespace Stamps.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly StampsDbContext context;
+        private readonly IMapper mapper;
+
+        public CategoriesController(StampsDbContext context, IMapper mapper) 
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet("/api/categories")]
+        public async Task<IEnumerable<CategoryResource>> GetCategoriesAsync() {
+            var categories = await context.Categories.ToListAsync();
+
+            return mapper.Map<List<Category>, List<CategoryResource>>(categories);
+        }
+    }
+}
